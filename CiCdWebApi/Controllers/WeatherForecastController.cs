de6ba13e068fa40015e6d970139d514fd4b63ed8
igoros00 <igor.osiemnicki@ingraphic.no>
2024-02-20 18:40:20 +0100

    new secret update

--- CiCdWebApi/Controllers/WeatherForecastController.cs
+++ CiCdWebApi/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace CiCdWebApi.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
